50333aca8df62890e2cc75e4d7feab2cdbc49895
PeggyHsiao <peggy10346011@gmail.com>
2022-05-17 22:50:31 +0800

    [ch5] LINQ

--- ch5/slnLINQ/slnLINQ/Controllers/DefaultController.cs
+++ ch5/slnLINQ/slnLINQ/Controllers/DefaultController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using slnLINQ.Models;
+
 namespace slnLINQ.Controllers
 {
     public class DefaultController : Controller
@@ -47,6 +49,31 @@ namespace slnLINQ.Controllers
 
             show += "<br />";
             show += "平均:" + sort.Average();
+
+            return show;
+        }
+
+        public string LoginMember(string uid, string pwd)
+        {
+            Member[] members = new Member[]
+            {
+                new Member { UId="tom", Pwd="123",Name="湯姆" },
+                new Member { UId="jasper", Pwd="456",Name="賈伯斯" },
+                new Member { UId="mary", Pwd="789",Name="瑪莉" }
+            };
+
+            var result = members.Where(m => m.UId == uid && m.Pwd == pwd).FirstOrDefault();
+
+            string show = "";
+
+            if (result != null)
+            {
+                show = result.Name + "歡迎進入系統";
+            }
+            else
+            {
+                show = "帳號密碼錯誤!";
+            }
 
             return show;
         }
--- ch5/slnLINQ/slnLINQ/Models/Member.cs
+++ ch5/slnLINQ/slnLINQ/Models/Member.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace slnLINQ.Controllers
+namespace slnLINQ.Models
 {
     public class Member
     {
